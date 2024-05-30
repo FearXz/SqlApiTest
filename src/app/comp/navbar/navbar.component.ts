335e449e79d40e40adff2473cd0e99795ced29b3
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-05-30 20:40:11 +0200

    refactor: Update navbar component HTML and add login link to navbar

--- src/app/comp/navbar/navbar.component.ts
+++ src/app/comp/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../../interfaces/User';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  constructor() {}
+export class NavbarComponent implements OnInit {
+  user: User | null = null;
+
+  constructor(private authSvc: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSvc.userCookie$.subscribe((user) => {
+      this.user = user;
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   constructor(private http: HttpClient) {
     this.checkCookie();
   }
-
+  // il metodo login prende un oggetto di tipo Login e restituisce un Observable di tipo boolean
   login(loginData: Login): Observable<boolean> {
     return this.http.post<User>(`${URL}/auth/login`, loginData).pipe(
       map((user) => {
@@ -33,12 +33,19 @@ export class AuthService {
       })
     );
   }
+  // il metodo logout rimuove il cookie 'user' e imposta user a null
+  logout(): void {
+    Cookies.remove('user');
+    this.user = null;
+    this.userCookie$.next(this.user);
+  }
 
+  //isAuthenticated() restituisce un booleano che indica se l'utente è autenticato o meno
   isAuthenticated(): boolean {
     const user = Cookies.get('user');
     return user ? true : false;
   }
-
+  // checkCookie() controlla se esiste un cookie con il nome 'user' e se esiste lo imposta come valore di userCookie$
   checkCookie(): void {
     const user = Cookies.get('user');
     if (user) this.userCookie$.next(JSON.parse(user));
