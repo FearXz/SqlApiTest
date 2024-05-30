2b5d59fff7b14260a0975ac381df571f293b9bf2
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-05-30 17:16:59 +0200

    feat: Add login route and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './comp/navbar/navbar.component';
+import { LoginComponent } from './comp/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
